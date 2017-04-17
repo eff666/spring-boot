6177240a475b5ce683957a1c77f4ad6537b23954
huangxiuyuan <xiuyuan.huang@shuyun.com>
2017-04-17 14:16:26 +0800

    spring-boot-scheduler

--- spring-boot-scheduler/src/main/java/com/demo/task/Scheduler1Task.java
+++ spring-boot-scheduler/src/main/java/com/demo/task/Scheduler1Task.java
@@ -3,20 +3,14 @@ package com.demo.task;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
-
-/**
- * Created by summer on 2016/12/1.
- */
-
 @Component
-public class SchedulerTask {
+public class Scheduler1Task {
 
     private int count=0;
 
     @Scheduled(cron="*/6 * * * * ?")
     private void process(){
-        System.out.println("this is scheduler task runing  "+(count++));
+        System.out.println("this is scheduler task running  " + count++);
     }
 
 }
